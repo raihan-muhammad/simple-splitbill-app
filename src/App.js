930a36fbe3064798ebb28546635e7cf45437652e
raihan-muhammad <sukasukangoding@gmail.com>
2024-01-13 00:00:12 +0700

    feat: add handler function when click add friend button

--- src/App.js
+++ src/App.js
@@ -6,12 +6,17 @@ import { useState } from "react";
 
 export default function App() {
   const [showAddFriend, setShowAddFriend] = useState(false);
+
+  const handleShowAddFriend = () => {
+    setShowAddFriend(!showAddFriend);
+  };
+
   return (
     <div className="app">
       <div className="sidebar">
         <FriendList />
         {showAddFriend && <FormAddFriend />}
-        <Button onClick={() => setShowAddFriend(!showAddFriend)}>
+        <Button onClick={handleShowAddFriend}>
           {showAddFriend ? "close" : "Add Friend"}
         </Button>
       </div>
